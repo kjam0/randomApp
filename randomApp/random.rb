060b0318395ee7859bf07c7ee30718f46a222e61
Kevin Mok <south.kay@gmail.com>
2021-02-28 18:23:21 +1100

    Added case when

--- randomApp/random.rb
+++ randomApp/random.rb
@@ -1,14 +1,25 @@
+def randomNummberGenerator
 p "Hi there, welcome to my random number generator."
 p "Please type 'roll' or 'exit':"
 
 i = 0
-while i <= 10
+while i <= 100
 user_input = gets.chomp.downcase
-if user_input == "roll"
+case user_input
+when "roll"
   p rand(100)
   i += 1
-elsif user_input == 'exit'
+  p "Please type 'roll' or 'exit': "
+when 'exit'
   p "See ya!"
   break
+else p "Error. Not a valid command"
 end
 end
+end
+
+def randomArray
+end
+
+
+randomNummberGenerator
